be2e9bc4b3a83e5b09d4a0ebcc2f4c1450e06939
martinoalanza03 <lanzamartino3@gmail.com>
2025-07-03 12:20:21 +0200

    feat: add costructor, method and attribute in Television

--- src/org/lessons/java/Television.java
+++ src/org/lessons/java/Television.java
@@ -1,5 +1,39 @@
 package org.lessons.java;
 
-public class Television {
+import java.math.BigDecimal;
+
+public class Television extends Product {
+    public float inch;
+    public boolean smart;
+
+    public Television(String name, String brand, BigDecimal price, float inch, boolean smart) {
+        super(name, brand, price);
+
+        this.inch = inch;
+        this.smart = smart;
+    }
+
+    public float getInch() {
+        return inch;
+    }
+
+    public void setInch(float inch) {
+        this.inch = inch;
+    }
+
+    public boolean getSmart() {
+        return smart;
+    }
+
+    public void setSmart(boolean smart) {
+        this.smart = smart;
+    }
+
+    public String isSmart() {
+        if (smart) {
+            return "La Tv è smart";
+        }
+        return "La Tv non è smart";
+    }
 
 }

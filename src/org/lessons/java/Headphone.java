fde591809d4238b11d1b140222d850fa5d24eccb
martinoalanza03 <lanzamartino3@gmail.com>
2025-07-03 12:33:00 +0200

    feat: add costructor, attribute and method for Headphone

--- src/org/lessons/java/Headphone.java
+++ src/org/lessons/java/Headphone.java
@@ -1,5 +1,32 @@
 package org.lessons.java;
 
-public class Headphone {
+import java.math.BigDecimal;
+
+public class Headphone extends Product {
+    public String color;
+    public boolean wireless;
+
+    public Headphone(String name, String brand, BigDecimal price, String color, boolean wireless) {
+        super(name, brand, price);
+
+        this.color = color;
+        this.wireless = wireless;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isWireless() {
+        return wireless;
+    }
+
+    public void setWireless(boolean wireless) {
+        this.wireless = wireless;
+    }
 
 }
